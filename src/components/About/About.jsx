aceabd74c5f36c4418231bb82ff5d933da67f082
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-09-11 21:42:00 -0400

    Updated About section and various styling changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,33 @@
+import "./About.scss"
+import profilePic from "../../assets/avatar.png"
+
 export default function About() {
     return (
-        <h1>This is the about section</h1>
+        <section className="about">
+            <div className="about-image">
+                <img src={profilePic} alt="profilePic" />
+            </div>
+            <div className="about-text">
+                <h1>About Me</h1>
+                <p>
+                    I'm Dillon Bellefeuille, a passionate game programmer specializing in Unreal Engine development, with a particular focus on UI engineering. With a deep understanding of Unreal Engine's framework and a keen eye for user experience, I design and implement intuitive, responsive, and engaging interfaces that enhance gameplay and immerse players in their worlds.
+                </p>
+                <p>
+                    Over the years, I have honed my skills in crafting modular and scalable systems, including inventory management, quest systems, and menu designs. My expertise lies in seamlessly integrating functionality with aesthetics, ensuring that every interface not only works flawlessly but also complements the overall game design.
+                </p>
+                <p>
+                    Whether working on core gameplay mechanics or polishing the finer details of a HUD, I thrive on solving complex challenges and bringing creative visions to life. I am always eager to collaborate with teams that value innovation, quality, and user-centric design.
+                </p>
+                <p>
+                    When I’m not coding, you’ll find me exploring the latest trends in game development, experimenting with new tools and techniques, or playing games that inspire my next big idea.
+                </p>
+                <p>
+                Some things I've worked on can be found on the Projects page. 
+                </p>
+                <p>
+                    I'm currently looking for a job in UI engineering. You can reach me at bellefeuilledillon@gmail.com or contact me here.
+                </p>
+            </div>
+        </section>
     )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.scss"
 export default function Footer() {
     return (
         <footer className="footer">
-            <h1>This is the footer</h1>
+            <p>This is the footer</p>
             {/* <a href="mailto:bellefeuilledillon@gmail.com">bellefeuilledillon@gmail.com</a> */}
             <a>bellefeuilledillon@gmail.com</a>
         </footer>
