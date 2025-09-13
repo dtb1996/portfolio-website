ac96ba693784bab3570a19ee63593158e49e9906
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-09-12 21:34:10 -0400

    Added copyright message and enabled mailto link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import "./Footer.scss"
 
 export default function Footer() {
+    const currentYear = new Date().getFullYear()
+
     return (
         <footer className="footer">
-            <p>This is the footer</p>
-            {/* <a href="mailto:bellefeuilledillon@gmail.com">bellefeuilledillon@gmail.com</a> */}
-            <a>bellefeuilledillon@gmail.com</a>
+            <p>Copyright &copy; {currentYear} Dillon Bellefeuille. All rights reserved.</p>
+            <a href="mailto:bellefeuilledillon@gmail.com">bellefeuilledillon@gmail.com</a>
         </footer>
     )
 }
