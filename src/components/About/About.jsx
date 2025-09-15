5c62393d3126763436d9432662b61deee51a3dbc
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-09-15 17:14:19 -0400

    Updated About section layout and styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,11 +4,9 @@ import profilePic from "../../assets/avatar.png"
 export default function About() {
     return (
         <section id="about" className="about">
-            <div className="about-image">
+            <h1>About Me</h1>
+            <div className="about-details">
                 <img src={profilePic} alt="profilePic" />
-            </div>
-            <div className="about-text">
-                <h1>About Me</h1>
                 <p>
                     I'm Dillon Bellefeuille, a passionate game programmer specializing in Unreal Engine development, with a particular focus on UI engineering. With a deep understanding of Unreal Engine's framework and a keen eye for user experience, I design and implement intuitive, responsive, and engaging interfaces that enhance gameplay and immerse players in their worlds.<br /><br />
 
