9c9fd364eeaeb05a7b89840cbcce9b881aebc5b6
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-09-15 17:30:43 -0400

    Added contact links to footer and updated styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { FaEnvelope, FaGithub, FaItchIo, FaLinkedin } from "react-icons/fa"
 import "./Footer.scss"
 
 export default function Footer() {
@@ -5,8 +6,21 @@ export default function Footer() {
 
     return (
         <footer className="footer">
-            <p>Copyright &copy; {currentYear} Dillon Bellefeuille. All rights reserved.</p>
-            <a href="mailto:bellefeuilledillon@gmail.com">bellefeuilledillon@gmail.com</a>
+            <p>Copyright &copy; {currentYear} Rolling Pixels. All rights reserved.</p>
+            <div className="footer-links">
+                <a href="mailto:bellefeuilledillon@gmail.com" target="blank" rel="noopener noreferrer">
+                    <FaEnvelope />
+                </a>
+                <a href="https://www.linkedin.com/in/dillon-bellefeuille/" target="blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/dtb1996" target="blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+                <a href="https://dillionaire.itch.io/" target="blank" rel="noopener noreferrer">
+                    <FaItchIo />
+                </a>
+            </div>
         </footer>
     )
 }
